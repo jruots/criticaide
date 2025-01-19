9ca54907439ddaeb39832e5413560010a276188e
jruots <joni.ruotsalainen@hotmail.com>
2025-01-19 14:50:59 +0200

    Added an initial setup screen
    Shows on first time startup

--- preload.js
+++ preload.js
@@ -9,4 +9,16 @@ contextBridge.exposeInMainWorld('api', {
     onAnalysisStart: (callback) => {
         ipcRenderer.on('analysis-start', () => callback());
     },
+
+    onInitialSetup: (callback) => {
+        ipcRenderer.on('show-initial-setup', () => callback());
+    },
+    
+    onSetupComplete: (callback) => {
+        ipcRenderer.on('initial-setup-complete', () => callback());
+    },
+    
+    onSetupError: (callback) => {
+        ipcRenderer.on('initial-setup-error', (event, error) => callback(error));
+    },
 });
